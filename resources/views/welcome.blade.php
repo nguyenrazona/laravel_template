35410b53b59242ddca6f7696e5ba8f1e25722171
Nguyen <nguyen@razona.jp>
2023-04-13 13:02:05 +0900

    :chore: install breeze

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -826,9 +826,6 @@
   </style>
 
   <link href="{{ mix('/css/app.css') }}" rel="stylesheet">
-  <script src="{{ mix('/js/manifest.js') }}"></script>
-  <script src="{{ mix('/js/vendor.js') }}"></script>
-  <script src="{{ mix('/js/app.js') }}"></script>
 </head>
 
 <body class="antialiased">
@@ -836,7 +833,7 @@
     @if (Route::has('login'))
       <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right">
         @auth
-          <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
+          <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
         @else
           <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
@@ -954,4 +951,7 @@
   </div>
 </body>
 
+<script src="{{ mix('/js/manifest.js') }}"></script>
+<script src="{{ mix('/js/vendor.js') }}"></script>
+<script src="{{ mix('/js/app.js') }}"></script>
 </html>
